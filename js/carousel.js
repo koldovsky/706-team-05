ce1247f781f009e56ba05eb1092ec41dc10dd84d
MashaNikolas <107943294+MashaN94@users.noreply.github.com>
2022-07-11 15:11:31 +0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -1,19 +1,1 @@
-const posts = [
-    {
-        name: "Jessica Peterson", 
-        post: "We have been cooperating with this company for a long time and order the construction of various commercial facillties quite often. This is a reliable contractor and a team of real professionals. For 10 years of cooperation, we have never encountered a violation of agreements.",
-        date: "December 15,2020"
-    },
-    {
-        name: "Daniel Williams", 
-        post: "Thanks to Great House, we have moved to our new home six months ago. We hesitated for a long time when choosing a contractor, as we wanted a non-standard design project. Great House specialists have won our trust and have come up with improvements that are really useful and convenient.",
-        date: "March 03,2021"
-    }, 
-    {
-        name: "Douglas Hamilton", 
-        post: "This is nit the first time we have worked with Gret House and every time, we are completely satisfied with the result. Everything is really great, from the development of a design project to the timing. They pay much attention to the eco materials and small details. And each project s perfect.",
-        date: "September 22,2021"
-    }
-]
-let counter = 0;
 

6927ebe6606b68396a2a71077c20388fa651f835
MashaN94 <nikolasmasha94@gmail.com>
2022-07-15 15:19:50 +0000

    completion of work on the carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,1 +1,28 @@
+const rigth = document.getElementById('carousel__bth_right')
+const left = document.getElementById('carousel__bth_left')
+
+let counter = 1;
+
+window.addEventListener('resize', () => {
+    console.log('resize')
+    counter = 1
+    window.location =`/#carousel${counter}`;
+})
+
+
+
+left.addEventListener('click', () => {
+    console.log('CLICK');
+    if (counter > 1)  {
+        counter--
+    }
+    window.location =`/#carousel${counter}`;
+})
+
+rigth.addEventListener('click', () => {
+    if (counter < 4)  {
+        counter++
+    }
+    window.location =`/#carousel${counter}`;
+})
 

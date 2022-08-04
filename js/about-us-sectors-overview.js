e6ceaa8462dfd6d43f3e0e1c3c22f1d2fb45f9d7
KseniiaOgo <ksohorodnyk@gmail.com>
2022-08-04 19:18:40 +0300

    Added css to sectors review section

--- js/about-us-sectors-overview.js
+++ js/about-us-sectors-overview.js
@@ -3,39 +3,39 @@
     const slides = [
                 `<div class="sectors-overview-slide">
                     <img class="sectors-overview-slide-commercial-img" src="./img/sectors-commercial.png" alt="hi-tech blue commercial house">
-                    <h3 class="sectors-overview-slide-commercial-header">Commercial</h3>
-                    <p class="sectors-overview-slide-commercial-content">We are experts in designing and building commercial objects.</p>
-                    <a href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
+                    <p class="sectors-overview-slide-header">Commercial</p>
+                    <p class="sectors-overview-slide-content">We are experts in designing and building commercial objects.</p>
+                    <a class="sectors-overview-learn-more" href="#">Learn more<span class="sectors-overview-learn-more-arrow">  &#8594;</span></a>
                 </div>`,
                 `<div class="sectors-overview-slide">
                     <img class="sectors-overview-slide-interiors-img" src="./img/sectors-interiors.png" alt="cozy open kitchen with wooden floor, comfort sofa and big black fridge">
-                    <h3 class="sectors-overview-slide-interiors-header">Interiors</h3>
-                    <p class="sectors-overview-slide-interiors-content">Need a unique interior design? We can turn any idea into reality.</p>
-                    <a href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
+                    <p class="sectors-overview-slide-header">Interiors</p>
+                    <p class="sectors-overview-slide-content">Need a unique interior design? We can turn any idea into reality.</p>
+                    <a class="sectors-overview-learn-more" href="#">Learn more<span class="sectors-overview-learn-more-arrow">  &#8594;</span></a>
                 </div>`,
                 `<div class="sectors-overview-slide">
                     <img class="sectors-overview-slide-residential-img" src="./img/sectors-residential.png" alt="beautiful residential house">
-                    <h3 class="sectors-overview-slide-residential-header">Residential</h3>
-                    <p class="sectors-overview-slide-residential-content">We are proud to build dream houses to our community.</p>
-                    <a href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
+                    <p class="sectors-overview-slide-header">Residential</p>
+                    <p class="sectors-overview-slide-content">We are proud to build dream houses to our community.</p>
+                    <a class="sectors-overview-learn-more" href="#">Learn more<span class="sectors-overview-learn-more-arrow">  &#8594;</span></a>
                 </div>`,
                 `<div class="sectors-overview-slide">
                     <img class="sectors-overview-slide-civic-img" src="./img/sectors-civil.png" alt="beautiful old-fashioned stone cultural building">
-                    <h3 class="sectors-overview-slide-civic-header">Civic and Cultural</h3>
-                    <p class="sectors-overview-slide-civic-content">We are happy to build public spaces that can make an impact.</p>
-                    <a href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
+                    <p class="sectors-overview-slide-header">Civic and Cultural</p>
+                    <p class="sectors-overview-slide-content">We are happy to build public spaces that can make an impact.</p>
+                    <a class="sectors-overview-learn-more" href="#">Learn more<span class="sectors-overview-learn-more-arrow">  &#8594;</span></a>
                 </div>`,
                 `<div class="sectors-overview-slide">
                     <img class="sectors-overview-slide-healthcare-img" src="./img/sectors-healthcare.png" alt="modern glass building">
-                    <h3 class="sectors-overview-slide-healthcare-header">Healthcare</h3>
-                    <p class="sectors-overview-slide-healthcare-content">We build hospitals, first-aid stations, and other objects.</p>
-                    <a href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
+                    <p class="sectors-overview-slide-header">Healthcare</p>
+                    <p class="sectors-overview-slide-content">We build hospitals, first-aid stations, and other objects.</p>
+                    <a class="sectors-overview-learn-more" href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
                 </div>`,
                 `<div class="sectors-overview-slide">
                     <img class="sectors-overview-slide-government-img" src="./img/sectors-government.png" alt="government headquaters">
-                    <h3 class="sectors-overview-slide-government-header">Government</h3>
-                    <p class="sectors-overview-slide-government-content">We provide professional services to the federal government.</p>
-                    <a href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
+                    <p class="sectors-overview-slide-header">Government</p>
+                    <p class="sectors-overview-slide-content">We provide professional services to the federal government.</p>
+                    <a class="sectors-overview-learn-more" href="#">Learn more<span class="sectors-overview-learn-more-arrow"> &#8594;</span></a>
                 </div>`,
     ];
 
@@ -48,7 +48,7 @@
         const secondSlideIdx = 
         currentSlideIdx + 1 >= slides.length ? 0 : currentSlideIdx + 1;
         slideContainer.innerHTML += slides[secondSlideIdx];
-        if (window.innerWidth > 800) {
+        if (window.innerWidth > 900) {
           const thirdSlideIdx =
             secondSlideIdx + 1 >= slides.length ? 0 : secondSlideIdx + 1;
           slideContainer.innerHTML += slides[thirdSlideIdx];

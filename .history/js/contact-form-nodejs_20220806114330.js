3dc5faa30c1095614ff02840f92b22384216b466
tetlisna <tplovic@gmail.com>
2022-08-06 14:18:14 +0300

    fixed errors

---
+++ .history/js/contact-form-nodejs_20220806114330.js
@@ -0,0 +1,34 @@
+(function () {
+   const form = document.querySelector(".form-contact");
+   
+   function handleSubmit(event) {
+       if (!form.checkValidity()) return;{
+     event.preventDefault();
+     const status = document.querySelector(".contact-formspree-status");
+     fetch("contacts/", {
+       method: "POST",
+       body: new FormData(form),
+       headers: {
+           'Accept': 'application/json'
+       }
+     }).then(response => {
+       if (response.ok) {
+         status.innerHTML = "Thanks for your submission!";
+         form.reset();
+       } else {
+         response.json().then(data => {
+           if (Object.hasOwn(data, 'errors')) {
+             status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+           } else {
+             status.innerHTML = "Oops! There was a problem submitting your form"
+           }
+         })
+       }
+     }).catch(error => {
+       status.innerHTML = "Oops! There was a problem submitting your form"
+     });}
+   }
+   form.addEventListener("submit", handleSubmit)
+   document.querySelector(".btn-close-ok").addEventListener('click', handleSubmit)
+
+})();
---
+++ .history/js/contact-form-nodejs_20220806133256.js
@@ -0,0 +1,62 @@
+(function () {
+  const form = document.querySelector(".form-contact");
+
+  function handleSubmit(event) {
+    if (!form.checkValidity()) return;
+    event.preventDefault();
+    const status = document.querySelector(".contact-nodejs-status");
+    fetch("/contacts", {
+      method: "POST",
+      body: JSON.stringify({
+        name: document.querySelector(
+          "#contact-form > div > div > section > div.contact-form-container > form > input[name = user-name]"
+        ).value,
+        tel: document.querySelector(
+          "#contact-form > div > div > section > div.contact-form-container > form > input[name = user-tel]"
+        ).value,
+        email: document.querySelector(
+          "#contact-form > div > div > section > div.contact-form-container > form > input[name = user-email]"
+        ).value,
+        comment: document.querySelector("#message").value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          //
+          // status.innerHTML = "Thanks for your submission!";
+          document
+            .querySelector(".contact-form-container")
+            .classList.add("nonactive");
+          document
+            .querySelector(".contact-form-message")
+            .classList.add("active");
+
+          form.reset();
+          console.log("ok");
+        } else {
+          response.json().then((data) => {
+            if (Object.hasOwn(data, "errors")) {
+              status.innerHTML = data["errors"]
+                .map((error) => error["message"])
+                .join(", ");
+            } else {
+              status.innerHTML =
+                "Oops! There was a problem submitting your form";
+            }
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        status.innerHTML = "Oops! There was a problem submitting your form";
+      });
+  }
+  // form.addEventListener("submit", handleSubmit)
+  document
+    .querySelector(".btn-submit-nodejs")
+    .addEventListener("click", handleSubmit);
+})();

4f0cb6bae93827f765ad8bd2ba5388ccda0cb119
Sofiia <lohin.sofia@gmail.com>
2022-08-09 01:29:21 +0300

    updating 8 part

---
+++ js/video.js
@@ -0,0 +1,67 @@
+
+/*
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+
+})
+
+
+const parallax = document.getElementById('parallax');
+
+window.addEventListener("scroll", function(){
+    let offset = window.pageYOffset;
+    parallax.forEach(function(prllx, i){
+        prllx.style.backgroundPositionY = (offset - prllx.offsetTop)*0.7+"px";
+    })
+})
+
+
+window.addEventListener('scroll', function(e){
+    const target = this.document.querySelector('.video-sec');
+
+    console.log()
+})
+
+
+
+
+function parallax() {
+	var $video = document.getElementById("video");
+
+	var yPos = window.pageYOffset / $video.dataset.speed;
+	yPos = -yPos;
+	
+	var coords = '0% '+ yPos + 'px';
+	
+	$video.style.backgroundPosition = coords;
+}
+
+window.addEventListener("scroll", function(){
+	parallax();	
+});
+
+
+
+function parallax() {
+	var $slider = document.getElementById("slider");
+
+	var yPos = window.pageYOffset / $slider.dataset.speed;
+	yPos = -yPos;
+	
+	var coords = '0% '+ yPos + 'px';
+	
+	$slider.style.backgroundPosition = coords;
+}
+
+window.addEventListener("scroll", function(){
+	parallax();	
+});
+
+const parallax = document.getElementById('video-sec');
+window.addEventListener('scroll', function(){
+    let offset = window.pageYOffset;
+    parallax.style.backgroundPositionY = offset*0.7+"px";
+})
+*/
